9a7fcaa6196ead0f53716dd02b311efcd6dc46b4
Kong <rhdtn311@naver.com>
2022-12-01 00:11:06 +0900

    initialize

---
+++ src/main/java/com/book/booksaleservice/BookSaleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.book.booksaleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSaleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSaleServiceApplication.class, args);
+    }
+
+}
