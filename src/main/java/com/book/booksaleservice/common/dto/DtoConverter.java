bc7a8fcae1251954066fce2de727bea7350e57e0
Kong <rhdtn311@naver.com>
2022-12-02 03:07:10 +0900

    feat : Entity -> DTO 로 변환시키는 클래스 추가

---
+++ src/main/java/com/book/booksaleservice/common/dto/DtoConverter.java
@@ -0,0 +1,13 @@
+package com.book.booksaleservice.common.dto;
+
+import com.book.booksaleservice.book.domain.Book;
+import com.book.booksaleservice.book.dto.BookDTO;
+
+public class DtoConverter {
+
+    private DtoConverter() {}
+    
+    public static BookDTO.Res convertBookToResDTO(Book book) {
+        return new BookDTO.Res(book.getTitle(), book.getAuthor(), book.getPublisher(), book.getPrice());
+    }
+}
