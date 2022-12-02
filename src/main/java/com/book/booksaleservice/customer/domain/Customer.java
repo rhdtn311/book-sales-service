08f1aa26f4a1f67e4500e589fc3195a25eebf88c
Kong <rhdtn311@naver.com>
2022-12-03 03:15:04 +0900

    delete : Address 클래스 제거

--- src/main/java/com/book/booksaleservice/customer/domain/Customer.java
+++ src/main/java/com/book/booksaleservice/customer/domain/Customer.java
@@ -3,10 +3,22 @@ package com.book.booksaleservice.customer.domain;
 public class Customer {
     private Long id;
     private final String email;
-    private final Address address;
+    private final String address;
 
-    public Customer(String email, Address address) {
+    public Customer(String email, String address) {
         this.email = email;
         this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
     }
 }
