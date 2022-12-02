da3829a6f84124a50b8c5cdd19081e84e9f0d0ec
Kong <rhdtn311@naver.com>
2022-12-03 03:21:39 +0900

    chore : OrderBook price 필드 제거

--- src/main/java/com/book/booksaleservice/order_book/domain/OrderBook.java
+++ src/main/java/com/book/booksaleservice/order_book/domain/OrderBook.java
@@ -2,13 +2,29 @@ package com.book.booksaleservice.order_book.domain;
 
 public class OrderBook {
     private Long id;
-    private Long orderId;
-    private Long bookId;
-    private final long price;
-    private final int amount;
+    private final Long orderId;
+    private final Long bookId;
+    private int amount;
 
-    public OrderBook(long price, int amount) {
-        this.price = price;
+    public OrderBook(Long orderId, Long bookId, int amount) {
+        this.orderId = orderId;
+        this.bookId = bookId;
         this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public int getAmount() {
+        return amount;
     }
 }
