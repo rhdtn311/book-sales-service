0b4d209d12282ba922f43307be28d63cf934be09
Kong <rhdtn311@naver.com>
2023-01-06 17:45:08 +0900

    feat : GlobalExceptionAdvicer가 RuntimeException을 핸들링하는 메소드 추가
    - String 타입을 인자로 받는 ErrorResponseDTO 생성자 추가

--- src/main/java/com/book/booksaleservice/common/dto/response/ErrorResponseDTO.java
+++ src/main/java/com/book/booksaleservice/common/dto/response/ErrorResponseDTO.java
@@ -1,10 +1,15 @@
 package com.book.booksaleservice.common.dto.response;
 
 import com.book.booksaleservice.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
 
 public class ErrorResponseDTO extends ResponseDTO {
 
     public ErrorResponseDTO(ErrorCode errorCode) {
         super(errorCode.getStatus(), errorCode.getMessage());
+    }
+
+    public ErrorResponseDTO(String message) {
+        super(HttpStatus.INTERNAL_SERVER_ERROR.value(), message);
     }
 }
--- src/main/java/com/book/booksaleservice/common/exception/GlobalExceptionAdvisor.java
+++ src/main/java/com/book/booksaleservice/common/exception/GlobalExceptionAdvisor.java
@@ -1,6 +1,7 @@
 package com.book.booksaleservice.common.exception;
 
 import com.book.booksaleservice.common.dto.response.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +16,14 @@ public class GlobalExceptionAdvisor {
         return new ResponseEntity<>(
                 new ErrorResponseDTO(errorCode),
                 HttpStatusCode.valueOf(errorCode.getStatus())
+        );
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponseDTO> handleRuntimeException(final RuntimeException e) {
+        return new ResponseEntity<>(
+                new ErrorResponseDTO(e.getMessage()),
+                HttpStatusCode.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value())
         );
     }
 }
