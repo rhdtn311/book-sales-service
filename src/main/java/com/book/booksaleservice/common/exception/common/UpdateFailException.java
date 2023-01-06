dc79561d4bc961ede90aefe1206adfc8ddffe4cd
Kong <rhdtn311@naver.com>
2023-01-06 17:46:06 +0900

    feat : UpdateFailException 추가

---
+++ src/main/java/com/book/booksaleservice/common/exception/common/UpdateFailException.java
@@ -0,0 +1,7 @@
+package com.book.booksaleservice.common.exception.common;
+
+public class UpdateFailException extends RuntimeException {
+    public UpdateFailException(String message) {
+        super(message);
+    }
+}
